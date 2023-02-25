230656e11f988b099d61900f259c5270bb465168
RocqJones <jonesmbindyo@gmail.com>
2023-02-25 18:59:15 +0300

    rename

--- me-design/src/main/java/com/rocqjones/me_design/ListActivity.kt
+++ me-design/src/main/java/com/rocqjones/me_design/ListActivity.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.rocqjones.me_design.ui.theme.MeSDKTheme
 
-class MainActivity : ComponentActivity() {
+class ListActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Second screen")
                 }
             }
         }
