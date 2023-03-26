98ed680377f0d2a197dd3a16287129d7b78286d1
RocqJones <jonesmbindyo@gmail.com>
2023-03-26 16:51:34 +0300

    test CI rules check

--- me-design/src/main/java/com/rocqjones/me_design/screens/bottomNavAdaptive/AdaptiveNavActivity.kt
+++ me-design/src/main/java/com/rocqjones/me_design/screens/bottomNavAdaptive/AdaptiveNavActivity.kt
@@ -22,7 +22,7 @@ class AdaptiveNavActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android CI Rules")
+                    Greeting("Android CI Rules check")
                 }
             }
         }
